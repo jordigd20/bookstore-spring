3a07750591a386bed1264a92fa017dc4659d04f8
jordigd20 <jordigd8@gmail.com>
2024-10-03 13:28:25 +0200

    first commit

---
+++ src/main/java/dev/bookstore/bookstore_spring/BookstoreSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.bookstore.bookstore_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreSpringApplication.class, args);
+	}
+
+}
